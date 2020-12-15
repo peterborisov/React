0d85e87a84dc2b2d8454b95cec69a7280430d149
peterborisov <borisov.peter@yahoo.co.uk>
2020-12-15 15:23:49 +0200

    initial commit for MERN stash todo app

---
+++ mern-app/README.md
@@ -0,0 +1,29 @@
+## Todo application using MERN stack
+#### User can add, edit todo and list all todos. 
+
+### What you need:
+Node.js<br/> MongoDB<br/> Express<br/> React<br/>
+
+### How to run application
+
+#### Frontend
+```
+1. npm install
+2. npm install bootstrap
+3. npm install react-router-dom
+4. npm start(to start frontend)
+5. npm install axios
+```
+#### Backend
+```
+1. cd backend
+2. npm install express body-parser cors mongoose
+3. npm install -g nodemon
+4. nodemon server.js
+5. install mongoDB<br/> https://docs.mongodb.com/manual/administration/install-community/
+6. mongo(to start mongoDB)
+7. use todos(create DB)
+```
+
+Tutorial:
+https://codingthesmartway.com/the-mern-stack-tutorial-building-a-react-crud-application-from-start-to-finish-part-1/
---
+++ mern-app/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav">
+                <li>
+                  <Link to="/" className="nav-link">List Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
