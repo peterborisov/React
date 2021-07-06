b5da14e465cd79d8001757d85280d36ade0686e3
peterborisov <borisov.peter@yahoo.co.uk>
2021-07-06 09:17:51 +0300

    React Redux starting pack

---
+++ React-Redux-starting-pack/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import './App.css';
+import store from './redux/store';
+import UsersContainer from './components/UsersContainer';
+
+function App () {
+  return (
+    <Provider store={store}>
+      <div className='App'>
+        <UsersContainer />
+      </div>
+    </Provider>
+  )
+}
+
+export default App
